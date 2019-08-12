c293e268396bc048aafaedceff488694fa7ff90d
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-08-12 14:05:41 +1000

    seed gh-pages

---
+++ index.md
@@ -0,0 +1,44 @@
+# uqlibrary-menu
+
+[![Codeship Status for uqlibrary/uqlibrary-menu](https://app.codeship.com/projects/34fdfc90-f6c8-0136-788f-76436281e834/status?branch=polymer1.0)](https://app.codeship.com/projects/321073)
+[![Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-menu.svg)](https://david-dm.org/uqlibrary/uqlibrary-menu)
+[![Dev Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-menu/dev-status.svg)](https://david-dm.org/uqlibrary/uqlibrary-menu?type=dev)
+
+Element providing main navigation menu in Polymer v1
+
+Element features:
+
+- skip navigation for accessibility
+- loads menu items from json file
+- notifies client for menu items loaded and menu item selected
+
+Full documentation and demo can be found in [GitHub Pages](https://uqlibrary.github.io/uqlibrary-menu/uqlibrary-menu/).
+
+## Getting Started
+
+Install Node.JS and run the following in the project directory:
+
+```sh
+npm install -g bower web-component-tester polymer-cli
+npm install
+bower install
+```
+
+## Developing
+
+- Please adhere to the Polymer code style guide provided at [Style Guide](http://polymerelements.github.io/style-guide/).
+- Colors and common styles are imported (bower install) from [uqlibrary-styles](http://github.com/uqlibrary/uqlibrary-styles).
+- GitHub pages should be updated after every commit to `master` branch by running `bin/generate-gh-pages.sh`
+- A preview of the component can be viewed locally by running `npm start`. Use the second URL from the command output.
+
+## Example
+
+```html
+<uqlibrary-menu
+  user="{{user}}"
+  menu-file="{{appMenuFile}}"
+  on-selected-menu-item-changed="closeMenu"
+  on-menu-items-changed="menuReloaded"
+  on-logout-clicked="logout"
+></uqlibrary-menu>
+```
---
+++ uqlibrary-menu/elements/uqlibrary-menu.js
@@ -0,0 +1,181 @@
+/**
+ * By Jan-Willem Wisgerhof <j.wisgerhof@uq.edu.au>
+ */
+(function () {
+  Polymer({
+    is: 'uqlibrary-menu',
+    properties: {
+      /**
+       * The current route of the app
+       */
+      route: {
+        type: String,
+        value: 'home'
+      },
+      /**
+       * The base of all URLs within the UQL app
+       */
+      baseUrl: {
+        type: String,
+        value: ''
+      },
+      /**
+       * Whether to load the menu items automatically
+       * @type Object
+       */
+      autoLoad: {
+        type: Object,
+        value: true
+      },
+      /**
+       * Holds the applications used by the menu
+       */
+      applications: {
+        type: Object,
+        observer: '_applicationsChanged'
+      },
+      /**
+       * Holds the user's account
+       */
+      _account: {
+        type: Object,
+        value: {
+          hasSession: false
+        }
+      },
+      /**
+       * @type Object
+       */
+      showHeader: {
+        type: Object,
+        value: false
+      },
+      /**
+       * The header image to be used
+       */
+      headerImage: {
+        type: String,
+        value: ''
+      }
+    },
+    ready: function () {
+      var self = this;
+
+      // Load the user's account
+      this.$.apiAccount.addEventListener('uqlibrary-api-account-loaded', function (e) {
+        if (e.detail.hasSession) {
+          self._account = e.detail;
+          self.$.apiApplications.get();
+
+          if (self.headerImage && self.showHeader) {
+            self.$$('.account').style.background = "url(" + self.headerImage + ") 100%";
+          }
+        }
+      });
+
+      // Load the Applications
+      this.$.apiApplications.addEventListener('uqlibrary-api-applications-loaded', function(e) {
+        self.applications = e.detail;
+      });
+
+      if (this.autoLoad) {
+        this.$.apiAccount.get();
+        this.$.apiApplications.get();
+      }
+    },
+    /**
+     * Called when a menu item has been clicked
+     * @param e
+     * @private
+     */
+    _menuItemClicked: function (e) {
+      this.fire("uqlibrary-menu-item-clicked", e.detail);
+    },
+    /**
+     * Called when applications have changed. Sets external / internal link
+     * @private
+     */
+    _applicationsChanged: function () {
+      for (var i = 0; i < this.applications.length; i++) {
+        var item = this.applications[i];
+
+        if (item.isDivider) {
+          item.isLink = false;
+        } else {
+          item.isLink = true;
+
+          if (item.link.indexOf("http") == 0) {
+            item.isExternal = true;
+            item.target = (item.target ? item.target : '_self');
+          } else {
+            item.isExternal = false;
+            item.route = item.link.substring(1);
+            item.href = this.baseUrl + item.route;
+          }
+        }
+      }
+      
+      this.fire('uqlibrary-menu-loaded', this.applications);
+    },
+    /**
+     * Toggles the drawer panel of the main UQL app
+     * @private
+     */
+    _toggleDrawerPanel: function () {
+      this.fire('uqlibrary-toggle-drawer');
+    },
+    /**
+     * Returns whether the user is on a mobile device
+     * @returns {boolean}
+     * @private
+     */
+    _isMobile: function () {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    },
+    /**
+     * returns true if application before current application is a divider
+     * @param index
+     * @private
+     */
+    _hasDivider: function (index) {
+      if (index > 0 && this.applications.length > index && this.applications[index-1].isDivider) {
+        return 'divider';
+      }
+    },
+    /**
+     * Called when the "skipNavigation" button is focused
+     * @param e
+     * @private
+     */
+    _skipNavigationFocused: function (e) {
+      this.$$('.skip-navigation').style.left = 'calc(50% - 100px)';
+      this.$$('.skip-navigation').style.top = "20px";
+    },
+    /**
+     * Called when the "skipNavigation" button loses focus
+     * @param e
+     * @private
+     */
+    _skipNavigationBlurred: function (e) {
+      this.$$('.skip-navigation').style.left = '-20000px';
+      this.$$('.skip-navigation').style.top = "-20000px";
+    },
+    /**
+     * Skips navigation by moving to the "after" span. Will prefer to navigate to the Mega Menu's after
+     * @param e
+     * @private
+     */
+    _skipNavigation: function (e) {
+      this.$.content.focus();
+    },
+    /**
+     * Performs the logout
+     * @param e
+     * @private
+     */
+    _logout: function (e) {
+      this.fire("uqlibrary-menu-logout");
+      this.$.apiAccount.logout();
+    }
+  })
+})();
